f777492829d3d36763df5b92e231d0d564ec299d
Lee Chul <winfe79@hanmail.net>
2018-12-14 14:29:34 +0900

    initialization

---
+++ TextFieldClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldClass
+//
+//  Created by formathead on 14/12/2018.
+//  Copyright © 2018 formathead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
